e7ac9a08d11f79fd16fdc755d624f33d6602f261
ketan <ketansingla7988@gmail.com>
2025-10-09 17:08:16 +0530

    Code optimization, status updation

--- backend/models/application.model.js
+++ backend/models/application.model.js
@@ -18,6 +18,6 @@ const applicationSchema  = new mongoose.Schema({
         enum: ['pending', 'accepted', 'rejected'],
         default:"pending"
     }
-}, {timeseries:true})
+}, {timestamps:true})
 
 export const Application = mongoose.model("Application", applicationSchema);
