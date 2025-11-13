69ba1da67c15867127e4f081f4b56eede0937681
ketan <ketansingla7988@gmail.com>
2025-11-13 17:41:47 +0530

    minor changes made

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api/v1";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://jobverse-backend-fm05.onrender.com/api/v1";
 
 export const USER_API_END_POINT = `${API_BASE_URL}/user`;
 
@@ -7,5 +7,3 @@ export const JOB_API_END_POINT = `${API_BASE_URL}/job`;
 export const APPLICATION_API_END_POINT = `${API_BASE_URL}/application`;
 
 export const COMPANY_API_END_POINT = `${API_BASE_URL}/company`;
-
-// export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company/get";
